a982ff92eeaeda99d6784544d2e015bb6799135f
devanshugarg1994 <devanshugarg1994@gmail.com>
2018-06-08 22:25:09 +0530

    Blog initial

---
+++ src/posts/views.py
@@ -0,0 +1,87 @@
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render,get_object_or_404 ,redirect
+
+from .forms import PostForm
+from .models import Post
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		#print form.cleaned_data.get("title")
+		instance.save()
+		messages.success(request , "Successfully Created")
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	
+	context = {
+		"form" : form,
+		}
+	return render(request,"post_form.html",context)
+
+def post_detail(request, id=None):
+	instance = get_object_or_404(Post, id=id)
+	context = {
+	"title" : instance.title,
+	"instance" : instance,
+
+	}
+	return render(request,"post_detail.html",context)
+
+def post_list(request):
+
+	queryset_list = Post.objects.all() 
+	paginator = Paginator(queryset_list, 5) # Show 25 querysets per page
+	page_request_variable = "page"
+	page = request.GET.get(page_request_variable)
+
+
+	try:
+		querysets = paginator.page(page)
+	except PageNotAnInteger:
+	# If page is not an integer, deliver first page.
+		querysets = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+		querysets = paginator.page(paginator.num_pages)
+
+	context = {
+		"object_list" : querysets,
+		"title" : "List",
+		"page_request_variable" : page_request_variable,
+	}
+
+	return render(request,"post_list.html",context)
+	#return HttpResponse("<h1>List</h1>")
+
+
+
+
+
+def post_update(request,id=None):
+	instance = get_object_or_404(Post, id=id)
+	form = PostForm(request.POST or None, instance=instance)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request ,"Saved")
+		return HttpResponseRedirect(instance.get_absolute_url())
+
+	context = {
+	"title" : instance.title,
+	"instance" : instance, 
+	"form" : form,
+	 }
+	return render(request,"post_form.html",context)
+
+
+def post_delete(request,id=None):
+	instance = get_object_or_404(Post, id=id)
+	instance.delete()
+	messages.success(request ,"Deleted")
+	return redirect('posts:list')
+
+
