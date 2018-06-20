1ff77ff047c35ca0ccf47553d8cc9c56cedb37b1
devanshugarg1994 <devanshugarg1994@gmail.com>
2018-06-20 14:51:03 +0530

    FacebookComment

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,58 +1,59 @@
 from __future__ import unicode_literals
-from django.db import models
+
+from django.conf import settings
 from django.core.urlresolvers import reverse
-
+from django.db import models
 from django.db.models.signals import pre_save
+
 from django.utils.text import slugify
-
-
-def upload_location(instance , filename):
-	return "%s/%s" %(instance.id,filename)
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
 
 class Post(models.Model):
-	title = models.CharField(max_length= 120)
+	user= models.ForeignKey(settings.AUTH_USER_MODEL,default = 1)
+	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
-	image = models.ImageField(upload_to= upload_location,null=True,blank=True,
-					width_field='width_field',
-					height_field='height_field')
+	image = models.ImageField(upload_to=upload_location, 
+	    			null=True, 
+	    			blank=True, 
+	    			width_field="width_field", 
+	    			height_field="height_field")
 	height_field = models.IntegerField(default=0)
 	width_field = models.IntegerField(default=0)
 	content = models.TextField()
-	updated = models.DateTimeField(auto_now= True, auto_now_add= False)
-	timestamp = models.DateTimeField(auto_now= False, auto_now_add= True)
-
-
+	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
 	def __unicode__(self):
 		return self.title
-
 
 	def __str__(self):
 		return self.title
 
 	def get_absolute_url(self):
-		return reverse("posts:detail", kwargs={"slug":self.slug})
+		return reverse("posts:detail", kwargs={"slug": self.slug})
 
 	class Meta:
-		ordering = ["-timestamp", "-updated"]
-
-def create_slug(instance, new_slug= None):
-	slug = slugify(instance.title)
-	if new_slug is not None:
-		slug = new_slug
-	qs = Post.objects.filter(slug=slug).order_by("-id")
-	exists = qs.exists()
-	if exists:
-		new_slug = "%s-%s" %(slug, qs.first().id)
-		return create_slug(instance,new_slug=new_slug)
-	return slug
+		ordering = ["-timestamp","-updated"]
 
 
 
-def pre_save_reciever(sender, instance,*args,**kwargs):
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
 
-	if not instance.slug:
-		instance.slug = create_slug(instance)
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
 
 
-pre_save.connect(pre_save_reciever, sender=Post)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,9 +1,8 @@
 from urllib.parse import quote_plus
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
-from django.http import HttpResponse, HttpResponseRedirect,Http404
-from django.shortcuts import render,get_object_or_404 ,redirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render, get_object_or_404, redirect
 from .forms import PostForm
 from .models import Post
 
@@ -13,84 +12,76 @@ def post_create(request):
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
-		#print form.cleaned_data.get("title")
+		instance.user = request.user
 		instance.save()
-		messages.success(request , "Successfully Created")
+		# message success
+		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
-
 	context = {
-		"form" : form,
-		}
-	return render(request,"post_form.html",context)
+		"form": form,
+	}
+	return render(request, "post_form.html", context)
 
 def post_detail(request, slug=None):
 	instance = get_object_or_404(Post, slug=slug)
 	share_string = quote_plus(instance.content)
 	context = {
-	"title" : instance.title,
-	"instance" : instance,
-	"share_string" : share_string,
-
+		"title": instance.title,
+		"instance": instance,
+		"share_string": share_string,
 	}
-	return render(request,"post_detail.html",context)
+	return render(request, "post_detail.html", context)
 
 def post_list(request):
-
-	queryset_list = Post.objects.all() 
-	paginator = Paginator(queryset_list, 5) # Show 25 querysets per page
-	page_request_variable = "page"
-	page = request.GET.get(page_request_variable)
-
-
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 10) # Show 25 contacts per page
+	page_request_var = "page"
+	page = request.GET.get(page_request_var)
 	try:
-		querysets = paginator.page(page)
+		queryset = paginator.page(page)
 	except PageNotAnInteger:
-	# If page is not an integer, deliver first page.
-		querysets = paginator.page(1)
+		# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
 	except EmptyPage:
-	# If page is out of range (e.g. 9999), deliver last page of results.
-		querysets = paginator.page(paginator.num_pages)
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
 
 	context = {
-		"object_list" : querysets,
-		"title" : "List",
-		"page_request_variable" : page_request_variable,
+		"object_list": queryset, 
+		"title": "List",
+		"page_request_var": page_request_var
 	}
-
-	return render(request,"post_list.html",context)
-	#return HttpResponse("<h1>List</h1>")
+	return render(request, "post_list.html", context)
 
 
 
 
 
-def post_update(request,slug=None):
+def post_update(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
-	
 	instance = get_object_or_404(Post, slug=slug)
-	form = PostForm(request.POST or None,request.FILES or None, instance=instance)
+	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
-		messages.success(request ,"Saved")
+		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
 		return HttpResponseRedirect(instance.get_absolute_url())
 
 	context = {
-	"title" : instance.title,
-	"instance" : instance, 
-	"form" : form,
-	 }
-	return render(request,"post_form.html",context)
+		"title": instance.title,
+		"instance": instance,
+		"form":form,
+	}
+	return render(request, "post_form.html", context)
 
 
-def post_delete(request,slug=None):
+
+def post_delete(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
-	
 	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
-	messages.success(request ,"Deleted")
-	return redirect('posts:list')
-
-
+	messages.success(request, "Successfully deleted")
+	return redirect("posts:list")
