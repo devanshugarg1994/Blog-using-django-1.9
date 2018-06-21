8958b34f3f357d4f13f6c67c26e84a82d8524cdd
devanshugarg1994 <devanshugarg1994@gmail.com>
2018-06-21 13:06:49 +0530

    Search Feature

--- src/posts/models.py
+++ src/posts/models.py
@@ -5,7 +5,17 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import pre_save
 
+from django.utils import timezone
+
 from django.utils.text import slugify
+
+
+class PostManager(models.Manager):
+	def active(self,*args,**kwargs):
+		return  super(PostManager,self).filter(draft = False).filter(publish__lte = timezone.now())
+
+
+
 def upload_location(instance, filename):
     return "%s/%s" %(instance.id, filename)
 
@@ -26,6 +36,8 @@ class Post(models.Model):
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
+	objects = PostManager()
+
 	def __unicode__(self):
 		return self.title
 
@@ -37,6 +49,12 @@ class Post(models.Model):
 
 	class Meta:
 		ordering = ["-timestamp","-updated"]
+
+
+
+
+
+
 
 
 
